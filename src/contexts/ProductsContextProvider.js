b4dc4324c966c36dff39e5e2a12e7737e3aca0e7
Reza Asadi <rezaasadirezaasadi80@gmail.com>
2023-03-30 12:33:59 +0430

    Create ProductsContextProvider to send products data to all components

---
+++ src/contexts/ProductsContextProvider.js
@@ -0,0 +1,27 @@
+import React, { createContext, useState, useEffect } from "react";
+
+// API
+import { getProducts } from "../services/api";
+
+// Create Context
+const ProductsContext = createContext();
+
+const ProductsContextProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    //   Creating a fetchAPI() to add product data
+    const fetchAPI = async () => {
+      setProducts(await getProducts());
+    };
+
+    fetchAPI();
+  }, []);
+  return (
+    <ProductsContext.Provider value={products}>
+      {children}
+    </ProductsContext.Provider>
+  );
+};
+
+export default ProductsContextProvider;
