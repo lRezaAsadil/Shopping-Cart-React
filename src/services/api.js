bc13840a502a59ed3c24532174b1f75ac6e53b0c
Reza Asadi <rezaasadirezaasadi80@gmail.com>
2023-03-30 12:28:39 +0430

    Create api.js to get products from api

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = "https://fakestoreapi.com";
 
-export const getproducts = async () => {
+export const getProducts = async () => {
   const response = await axios.get(`${BASE_URL}/products`);
   return response.data;
 };
